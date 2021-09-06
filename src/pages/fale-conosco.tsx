02a3d1c3492c9dd54bb0c1b0bbb2a3e89a08ab0f
jcgsr <cabralguerra@gmail.com>
2021-09-05 21:40:13 -0300

    formulário

--- src/pages/fale-conosco.tsx
+++ src/pages/fale-conosco.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import {StaticImage} from "gatsby-plugin-image"
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const Supervisao = () => {
   return (
     <Layout>
-      <Seo title="Supervisão Profissional" description="Página sobre Supervisão Profissional" />
-    <main>
-      <article className="card">
-        <StaticImage src="../assets/images/fale.jpg" layout='constrained' placeholder="tracedSVG" alt="Camylle ao telefone"/>
+      <Seo
+        title="Fale Conosco"
+        description="Página para enviar mensagens para Camylle"
+      />
+      <main>
+        <article className="card">
+          <StaticImage
+            src="../assets/images/fale.jpg"
+            layout="constrained"
+            placeholder="tracedSVG"
+            alt="Camylle ao telefone"
+          />
 
-        <h1>fale conosco</h1>
-             
-      </article>
-      
-    </main>
+          <h1>fale conosco</h1>
+          <div className="form-container">
+            <form
+              action="https://formspree.io/f/xoqyykby"
+              method="POST"
+              className="form"
+            >
+              <label>
+                E-mail:
+                <input type="email" name="_replyto" />
+              </label>
+              <label>
+                Mensagem:
+                <textarea name="message"></textarea>
+              </label>
+
+              <button className="btn" type="submit">
+                Enviar
+              </button>
+            </form>
+          </div>
+        </article>
+      </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default Supervisao
+export default Supervisao;
